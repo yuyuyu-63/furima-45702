bd87d77446c2514c5b921ac74f6b01503f8848ad
yuyuyu-63 <yukino1999713@gmail.com>
2025-11-28 20:47:33 +0900

    ユーザー登録機能のパラメータ許可処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true,
+            format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, presence: true,
+            format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :birthday, presence: true
+
 end
