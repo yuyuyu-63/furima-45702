d6a47c4411bc204bf99f6d43984dc8725edf4206
yuyuyu-63 <yukino1999713@gmail.com>
2025-11-28 17:20:18 +0900

    Add ItemsController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'items/index'
 
   root to: "items#index"
-
+  resources :items
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
