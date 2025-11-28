09398fe6c6cfa0eac8a7c0e6a81d73b1ab96a559
yuyuyu-63 <yukino1999713@gmail.com>
2025-11-28 19:46:59 +0900

    Userモデル設定初期設定

--- db/migrate/20251128103758_devise_create_users.rb
+++ db/migrate/20251128103758_devise_create_users.rb
@@ -32,6 +32,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :nickname,        null: false
+      t.string :last_name,       null: false
+      t.string :first_name,      null: false
+      t.string :last_name_kana,  null: false
+      t.string :first_name_kana, null: false
+      t.date   :birthday,        null: false
+
+
+
 
       t.timestamps null: false
     end
